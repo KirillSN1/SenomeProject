f71ff4e01a549dfa7d71199d82febfd830541372
KirillSN1 <53705733+KirillSN1@users.noreply.github.com>
2019-11-19 21:12:27 +0300

    Изменено главное меню.(Можно нажать на Лого)

---
+++ Assets/Scripts/UI/ExpandLogo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandLogo : MonoBehaviour
+{
+    public float time;
+    public GameObject Title;
+    public GameObject StartPos;
+    public Transform EndPos;
+
+    private bool Expanded=false;
+
+    void FixedUpdate()
+    {
+        if (!Expanded)
+        {
+            Title.transform.position = Vector2.Lerp(Title.transform.position, EndPos.transform.position, Time.fixedDeltaTime*time);
+        }
+        else
+        {
+            Title.transform.position = Vector2.Lerp(Title.transform.position, StartPos.transform.position, Time.fixedDeltaTime*time);
+        }
+    }
+
+    public void Expand()
+    {
+        if (!Expanded)
+        {
+            Expanded = true;
+        }
+        else Expanded = false;
+    }
+}
