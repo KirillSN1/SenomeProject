d385f2dbbaec901cf87908bce242a3dd2863f0c7
kbarketova <kbarketova@list.ru>
2019-11-20 21:24:56 +0300

    Ледяные колья, цвет всплывающих окон

---
+++ Assets/Scripts/IceSpikes.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSpikes : MonoBehaviour
+{
+    public float TimeBetweenAttack = .2f;
+    public float TimeTillAttack;
+    public int DamageAmount = 1;
+
+    public bool IsOnSpike = false;
+
+    void Start()
+    {
+        TimeTillAttack = 0;
+    }
+
+    void Update()
+    {
+        if (IsOnSpike)
+        {
+            TimeTillAttack -= Time.deltaTime;
+
+            if (TimeTillAttack < 0)
+            {
+                TimeTillAttack = 0;
+            }
+        }
+
+        if (TimeTillAttack <= 0 && IsOnSpike)
+        {
+            HurtTarget();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsOnSpike = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        IsOnSpike = false;
+        TimeTillAttack = TimeBetweenAttack;
+    }
+
+    private void HurtTarget()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
+        StartCoroutine(player.ReceiveDamage(DamageAmount));
+
+        TimeTillAttack = TimeBetweenAttack;      
+    }
+}
