c19612656d885823605ac0c9446e7b290c674f04
KirillSN1 <53705733+KirillSN1@users.noreply.github.com>
2020-01-07 19:08:31 +0300

    Добавил зависимость автогенерации от положения ГГ.

--- Assets/Scripts/ChunkSpawn.cs
+++ Assets/Scripts/ChunkSpawn.cs
@@ -7,6 +7,8 @@ public class ChunkSpawn : MonoBehaviour
     public PlayerBehaviour Player;
     public Chunk[] Chunks;
     public List<Chunk> ChunksOnScane = new List<Chunk>();
+    [Range(0,30)]
+    public float SpawnDistance=15;
 
     void Start()
     {
@@ -16,7 +18,7 @@ public class ChunkSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Player.transform.position.x>= ChunksOnScane[ChunksOnScane.Count - 1].EndPos.position.x - SpawnDistance)
         {
             SpawnNewChunk();
         }
