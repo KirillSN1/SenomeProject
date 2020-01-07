309cd978d598cd447b7a682843479f7c15f98f87
KirillSN1 <53705733+KirillSN1@users.noreply.github.com>
2020-01-07 15:55:20 +0300

    Автогенерация участков уровня(пока по кнопке A)

---
+++ Assets/Scripts/ChunkSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkSpawn : MonoBehaviour
+{
+    public PlayerBehaviour Player;
+    public Chunk[] Chunks;
+    public List<Chunk> ChunksOnScane = new List<Chunk>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SpawnNewChunk();
+        }
+    }
+
+    void SpawnNewChunk()
+    {
+        Chunk newChunk = Instantiate(Chunks[Random.Range(0, Chunks.Length)]);
+        newChunk.transform.position = ChunksOnScane[ChunksOnScane.Count-1].EndPos.position - newChunk.StartPos.localPosition;
+        ChunksOnScane.Add(newChunk);
+
+    }
+}
