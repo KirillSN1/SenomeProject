6926165728948039c3ac08fe941c75882056a774
KirillSN1 <53705733+KirillSN1@users.noreply.github.com>
2020-01-10 16:53:47 +0300

    Автоматическое удаление чанков.

--- Assets/Scripts/ChunkSpawn.cs
+++ Assets/Scripts/ChunkSpawn.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ChunkSpawn : MonoBehaviour
 {
-    public PlayerBehaviour Player;
-    public Chunk[] Chunks;
-    public List<Chunk> ChunksOnScane = new List<Chunk>();
+    [Range(2,10)]
+    public int LimitOfChunks=5;
     [Range(0,30)]
     public float SpawnDistance=15;
+    public Chunk[] Chunks;
+    public List<Chunk> ChunksOnScane = new List<Chunk>();
+    private GameObject Player;
 
-    void Start()
+    void Awake()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -22,13 +24,23 @@ public class ChunkSpawn : MonoBehaviour
         {
             SpawnNewChunk();
         }
+        DeleteOldChunk();
     }
 
     void SpawnNewChunk()
     {
         Chunk newChunk = Instantiate(Chunks[Random.Range(0, Chunks.Length)]);
-        newChunk.transform.position = ChunksOnScane[ChunksOnScane.Count-1].EndPos.position - newChunk.StartPos.localPosition;
-        ChunksOnScane.Add(newChunk);
+        newChunk.transform.position = ChunksOnScane[ChunksOnScane.Count-1].EndPos.position - newChunk.StartPos.localPosition;        
+        ChunksOnScane.Add(newChunk);   
+    }
 
+    void DeleteOldChunk()
+    {
+        if (ChunksOnScane.Count > LimitOfChunks)
+        {
+            Destroy(ChunksOnScane[0].gameObject);
+            ChunksOnScane.RemoveAt(0);
+            Debug.Log("Delating");
+        }
     }
 }
