d0290f4422e959debcff89d42a591c3f157a0b35
KirillSN1 <53705733+KirillSN1@users.noreply.github.com>
2020-02-01 13:58:52 +0300

    Добавлен множитель(GameManager).

---
+++ Assets/Scripts/GamePlay/Multiplier.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Multiplier : MonoBehaviour
+{
+    public int MultiplyFactor;
+    public bool Multiplication;
+    public Text UIText;
+
+    private PlayerBehaviour Player;
+
+
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Multiplication)
+        {
+            MultiplicationOn();
+            UIChange();
+        }
+        else
+        {
+            UIText.gameObject.SetActive(false);
+        }
+    }
+
+    public void MultiplicationOn()
+    {
+        foreach (var PickUps in Player.GameObjectsinView)
+        {
+            PickUps.GetComponent<PickUps>().Value = MultiplyFactor;
+        }
+    }
+
+    public void UIChange()
+    {
+        UIText.gameObject.SetActive(true);
+        UIText.text = MultiplyFactor + "x";
+    }
+}
