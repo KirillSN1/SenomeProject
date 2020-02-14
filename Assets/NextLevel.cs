5c00298a4bcb8bd6209e3b9beaf4081fdc1d1e3e
ezheka <efim.zhenya.2000@gmail.com>
2020-02-15 00:31:25 +0500

    Переход на след. уровень
    Изменил коллайдер у глав. героя; Добавил в конце сцены триггер для перехода на след. уровень

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public int idScenes;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name== "Rabbit")
+        {
+            SceneManager.LoadScene(idScenes);
+        }
+    }
+}
