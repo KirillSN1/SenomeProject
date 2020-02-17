1dfa42afdfbae9cf453493706d1a45d8d7e76c3e
Ivan <imanchini@list.ru>
2020-02-17 22:39:27 +0700

    Звуки + финиш
    Звуки ходьбы + тест финиш 2 лвл

---
+++ Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevel : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayPanel;
+    [SerializeField] private GameObject WinPanel;
+    [SerializeField] private GameObject Player;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player")
+        {
+            Win();
+        }
+    }
+    private void Win()
+    {
+        PlayPanel.SetActive(false);
+        WinPanel.SetActive(true);
+        Player.SetActive(false);
+    }
+}
