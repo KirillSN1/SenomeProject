b12be72c67dc2a6ce66b2c71127739342e435e89
Stern_989 <dmitrievas3d@gmail.com>
2020-02-18 14:45:57 +0300

    Snow

---
+++ Assets/Snow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    public GameObject RabbitPlayer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //transform.Translate(new Vector3(RabbitPlayer.transform.position.x,34,-45));
+        gameObject.transform.position = new Vector3(RabbitPlayer.transform.position.x, 34, -45);
+
+
+    }
+}
